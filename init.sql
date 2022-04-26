35e6c0b71fc2c4296614a6c3e7a6bdc1d387b91c
slagga <957766762@qq.com>
2022-04-26 17:39:45 +0800

    add sub-account service

--- init.sql
+++ init.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXISTS `bit_database`;
 CREATE DATABASE IF NOT EXISTS `bit_account_indexer`;
 CREATE DATABASE IF NOT EXISTS `bit_register`;
+CREATE DATABASE IF NOT EXISTS `sub_account`;
